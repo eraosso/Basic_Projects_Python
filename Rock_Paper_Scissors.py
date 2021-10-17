ca636baf342ed9a1b1e43c48adc2e27b3f5bdb18
eraosso <osmar.sousa@gmail.com>
2021-10-17 18:08:30 -0300

    Final commit with logic implemented!

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,14 +4,14 @@ from random import randint
 
 opcoes = ['Rock', 'Paper', 'Scissors']
 
-player_wins = 0
-computer_wins = 0
+number_of_player_wins = 0
+number_of_computer_wins = 0
 games = 0
 
 def player():
     while True:
         try:
-            jogador = int(input("Please enter your choice: \n[0]-Rock\n[1]-Paper\n[2]-Scissors"))
+            jogador = int(input("Please enter your choice: \n[0]-Rock\n[1]-Paper\n[2]-Scissors\n"))
             if 0 <= jogador <= 2:
                 return jogador
             else:
@@ -23,15 +23,18 @@ def player():
 def computer():
     return randint(0, 2)
 
-def player_wins()
+
+def player_wins():
     print("Player wins!")
+    global games, number_of_player_wins
     games += 1
-    player_wins += 1
+    number_of_player_wins += 1
 
 def computer_wins():
     print("Computer wins!")
+    global games, number_of_computer_wins
     games += 1
-    computer_wins += 1
+    number_of_computer_wins += 1
 
 def decide(jogador, computador):
     if jogador < computador:
@@ -40,6 +43,7 @@ def decide(jogador, computador):
         computer_wins()
 
 def analisa(jogador, computador):
+    global games
     if jogador == computador:
         print("It's a tie! Try again!")
         games += 1
@@ -51,7 +55,17 @@ def analisa(jogador, computador):
         decide(jogador, computador)
 
 print("Welcome to the game!")
-player()
-continuar = ""
-while continuar.lower() == "s":
+continuar = "Y"
+while continuar.lower() == "y":
+    jogador = player()
+    computador = computer()
+    analisa(jogador, computador)
+    continuar = input("Do you want to play more? ('Y/N')")
+
+print("Total # of games: {}".format(games))
+print("Player wins: {} - {:.2%}".format(number_of_player_wins, number_of_player_wins / games))
+print("Computer wins: {} - {:.2%}".format(number_of_computer_wins, number_of_computer_wins / games))
+print("Total number of ties: {}".format(games - number_of_computer_wins - number_of_player_wins))
+
+
 
